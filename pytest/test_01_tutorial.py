81e56af3b7e0c05144c13f71c7ba36af701dace8
Tal Shaked <73061047+talshaked-git@users.noreply.github.com>
2024-09-15 11:28:56 +0300

    Update test_01_tutorial.py

--- pytest/test_01_tutorial.py
+++ pytest/test_01_tutorial.py
@@ -15,3 +15,15 @@ def test_divide_by_zero():
 
     assert 'division by zero' in str(e.value)
     assert e.type == ZeroDivisionError
+
+test_data = [
+    (2, 3, 6),
+    (99, 1, 99),
+    (0, 10, 0),
+    (-5, 1, -5),
+    (-5, -2, 10)
+]
+
+@pytest.mark.parametrize('num1, num2, result', test_data)
+def test_multipication(num1, num2, result):
+    assert num1 * num2 == result
